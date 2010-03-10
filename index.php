5c95faf3c1635b2bf169be62cea37730ae97c580
lancecore <lance+github@lancecore.com>
2010-03-10 01:59:13 -0700

    Sloppy...

--- index.php
+++ index.php
@@ -1,9 +1,176 @@
 <html>
 <head>
-<title>Statusarchy</title>
+	<title>Statusarchy</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
 </head>
+
 <body>
-<h1>Future home of Statusarchy!</h1>
+<div id="wrap">
+	
+<div id="graphs">
+	<div id="graphwrap">
+	<div id="graphbox1">
+		<span class="graphnums">18<span class="slash">/</span><em>1</em></span>
+		<div id="graph1"></div>
+		<span class="graphtitle">TRANSMIT</span>
+	</div>
+
+	<div id="graphbox2">
+		<span class="graphnums">29<span class="slash">/</span><em>3</em></span>
+		<div id="graph2"></div>
+		<span class="graphtitle">CODA</span>
+	</div>
+
+	<div id="graphbox3">
+		<span class="graphnums">48<span class="slash">/</span><em>3</em></span>
+		<div id="graph3"></div>
+		<span class="graphtitle">UNISON</span>
+	</div>
+
+	<div id="graphbox4">
+		<span class="graphnums">8<span class="slash">/</span><em>2</em></span>
+		<div id="graph4"></div>
+		<span class="graphtitle">CANDYBAR</span>
+	</div>
+	
+	<div id="graphbox5">
+		<span class="graphnums">3<span class="slash">/</span><em>0</em></span>
+		<div id="graph5"></div>
+		<span class="graphtitle">GENERAL</span>
+	</div>
+	</div>
+</div>
+
+<div id="projects">
+	<div class="projrow">
+		<span class="icon">*</span>
+		<span class="title">Transmit</span>
+		<span class="info">Info1</span>
+		<span class="info">Info2</span>
+		<span class="members">1 2 3 4</span>
+	</div>
+	
+	<div class="projrow">
+		<span class="icon">*</span>
+		<span class="title">Unison</span>
+		<span class="info">Info1</span>
+		<span class="info">Info2</span>
+		<span class="members">1 2 3 4</span>
+	</div>
+	
+	<div class="projrow">
+		<span class="icon">*</span>
+		<span class="title">Coda</span>
+		<span class="info">Info1</span>
+		<span class="info">Info2</span>
+		<span class="members">1 2 3 4</span>
+	</div>
+	
+	<div class="projrow">
+		<span class="icon">*</span>
+		<span class="title">Project4</span>
+		<span class="info">Info1</span>
+		<span class="info">Info2</span>
+		<span class="members">1 2 3 4</span>
+	</div>
+	
+	<div class="projrow">
+		<span class="icon">*</span>
+		<span class="title">Project5</span>
+		<span class="info">Info1</span>
+		<span class="info">Info2</span>
+		<span class="members">1 2 3 4</span>
+	</div>
+	
+	<div class="projrow">
+		<span class="icon">*</span>
+		<span class="title">Project6</span>
+		<span class="info">Info1</span>
+		<span class="info">Info2</span>
+		<span class="members">1 2 3 4</span>
+	</div>
+	
+	<div class="projrow">
+		<span class="icon">*</span>
+		<span class="title">Project7</span>
+		<span class="info">Info1</span>
+		<span class="info">Info2</span>
+		<span class="members">1 2 3 4</span>
+	</div>
+	
+	<div class="projrow">
+		<span class="icon">*</span>
+		<span class="title">Project8</span>
+		<span class="info">Info1</span>
+		<span class="info">Info2</span>
+		<span class="members">1 2 3 4</span>
+	</div>
+	
+	<div class="projrow">
+		<span class="icon">*</span>
+		<span class="title">Project9</span>
+		<span class="info">Info1</span>
+		<span class="info">Info2</span>
+		<span class="members">1 2 3 4</span>
+	</div>
+	
+	<div class="projrow">
+		<span class="icon">*</span>
+		<span class="title">Project10</span>
+		<span class="info">Info1</span>
+		<span class="info">Info2</span>
+		<span class="members">1 2 3 4</span>
+	</div>
+</div>
+
+<div id="countdowns">
+	<span class="count">* 25 Days</span> <span class="event">until iPad Launch</span>
+</div>
+
+<div id="infoboxes">
+	<div id="profit">
+		<p class="triangle"></p>
+		<p>0.95<em>%</em></p>
+	</div>
+	
+	<div id="bus">
+		<div class="buswrap">
+			<div class="circle1">4</div><div class="time">1:02</div>
+		</div>
+		<div class="buswrap">
+			<div class="circle2">9</div><div class="time">9 Min</div>
+		</div>
+	</div>
+	
+	<div id="calendar">
+		<div id="calhead">Monday, 3<span class="slash">/</span>8</div>
+		<ul>
+			<li><span class="date">3<span class="slash">/</span>11</span> Ned Out (Vacati...</li>
+			<li><span class="date">3<span class="slash">/</span>12</span> Ned Out (Vacati...</li>
+			<li><span class="date">3<span class="slash">/</span>15</span> Ned Out (Vacati...</li>
+		</ul>
+	</div>
+</div>
+
+<div id="twitbar">
+
+	<div id="mytweets">
+		<ul>
+			<li>tv whatsoever - does that count?</li>
+			<li>* I confess: I didnt think much of hu</li>
+		</ul>
+	</div>
+	
+	<div id="othertweets">
+		<ul>
+			<li>the last 14+ years. I cant begin to say how much I love Coda.</li>
+			<li>* Healthy</li>
+		</ul>
+	</div>
+
+</div>
+
+</div>
 </body>
 </html>
-
